3bd0b380c29ffca58002b59048dc22a37132f596
Leopoldo Brines <leopoldo.brines@gmail.com>
2021-06-28 06:54:27 -0300

    Ajust image name

--- update-k8s.sh
+++ update-k8s.sh
@@ -4,4 +4,4 @@
 set -feu	# Usage: $0 <GIT_SHA>	# Fails when GIT_SHA is not provided
 
 GIT_SHA=$1
-sed -i "s|imagen: nginx:.*|imagen: nginx:$GIT_SHA|" k8s/deploy-ngix.yaml
+sed -i "s|imagen: lbrines/nginx:.*|imagen: lbrines/nginx:$GIT_SHA|" k8s/deploy-ngix.yaml
