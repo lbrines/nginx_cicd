e3a7d638ec863c21e5d32e3fbdf48f4a36192a56
Leopoldo Brines <leopoldo.brines@gmail.com>
2021-06-28 17:12:22 -0300

    Update deploy nginx image

--- update-k8s.sh
+++ update-k8s.sh
@@ -4,4 +4,4 @@
 set -feu	# Usage: $0 <GIT_SHA>	# Fails when GIT_SHA is not provided
 
 GIT_SHA=$1
-sed -i "s|imagen: lbrines/nginx:.*|imagen: lbrines/nginx:$GIT_SHA|" cluster/prueba-03/ns-prueba-03/deploy-ngix.yaml
+sed -i "s|imagen: lbrines/nginx-prueba03:.*|imagen: lbrines/nginx-prueba03:$GIT_SHA|" cluster/prueba-03/ns-prueba-03/deploy-nginx.yaml
